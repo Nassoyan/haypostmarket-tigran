b8c802d2ce96316504fdbca185550bf13a4282c5
Tigran <112520303+Nassoyan@users.noreply.github.com>
2023-03-14 11:03:33 +0400

    added hover method

--- Components/Description.js
+++ Components/Description.js
@@ -4,9 +4,6 @@ import { useState } from "react";
 import DescriptionImg from "../public/images/descriptionCards.png";
 
 export default function Description() {
-  const [hover, setHover] = useState(false);
-  const [hover1, setHover1] = useState(false);
-  const [hover2, setHover2] = useState(false);
   return (
     <div className="desc_wrapper">
       <div className="container description">
@@ -30,47 +27,17 @@ export default function Description() {
         </div>
       </div>
       <div className="desc_text">
-        <div
-          onMouseLeave={() => {
-            setHover(false);
-          }}
-          onMouseEnter={() => {
-            setHover(true);
-          }}
-        >
+        <div className="desc_text_inner">
           <span>Օրվա առաջարկ</span>
-          <div
-            style={{ visibility: !hover ? "hidden" : "visible" }}
-            className="each_desc_stroke"
-          ></div>
+          <div className="each_desc_strokee"></div>
         </div>
-        <div
-          onMouseLeave={() => {
-            setHover1(false);
-          }}
-          onMouseEnter={() => {
-            setHover1(true);
-          }}
-        >
+        <div className="desc_text_inner">
           <span>Պահանջված տեսականի</span>
-          <div
-            style={{ visibility: !hover1 ? "hidden" : "visible" }}
-            className="each_desc_stroke"
-          ></div>
+          <div className="each_desc_strokee"></div>
         </div>
-        <div
-          onMouseLeave={() => {
-            setHover2(false);
-          }}
-          onMouseEnter={() => {
-            setHover2(true);
-          }}
-        >
+        <div className="desc_text_inner">
           <span>Նոր տեսականի</span>
-          <div
-            style={{ visibility: !hover2 ? "hidden" : "visible" }}
-            className="each_desc_stroke"
-          ></div>
+          <div className="each_desc_strokee"></div>
         </div>
       </div>
     </div>
---
+++ public/svg/SmsIcon.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+function MessageIcon() {
+  return (
+    <svg width={20} height={16} viewBox="0 0 20 16" fill="none">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 1.25a1 1 0 011-1h18a1 1 0 011 1v13.5a1 1 0 01-1 1H1a1 1 0 01-1-1V1.25zm2 1v11.5h16V2.25H2z"
+        fill="#9098B1"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M.24.6A1 1 0 011.65.49L10 7.648 18.35.491a1 1 0 011.3 1.518l-9 7.714a1 1 0 01-1.3 0l-9-7.714A1 1 0 01.24.6z"
+        fill="#9098B1"
+      />
+    </svg>
+  );
+}
+
+export default MessageIcon;
